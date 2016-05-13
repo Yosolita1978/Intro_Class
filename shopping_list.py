f2affc6992ee4df452a11b8bb68f49e68d876ed0
Cristina Rodriguez <yosola@gmail.com>
2016-05-12 18:19:57 -0700

    Now the shopping list have a while loop

--- shopping_list.py
+++ shopping_list.py
@@ -1,52 +1,58 @@
-shoppin_list =[]
+shopping_list =[]
 
 def add(item):
-    global shoppin_list
-    if item.upper() in shoppin_list or item.lower() in shoppin_list:
+    global shopping_list
+    if item.upper() in shopping_list or item.lower() in shopping_list:
         print "That item already exist"
-        shoppin_list.sort()
+        shopping_list.sort()
     else:
-        shoppin_list.append(item)
-        return shoppin_list
+        shopping_list.append(item)
+        return shopping_list
 
 def remove(item):
-    global shoppin_list
-    if item.upper() in shoppin_list or item.lower() in shoppin_list:
-        shoppin_list.remove(item)
+    global shopping_list
+    if item.upper() in shopping_list or item.lower() in shopping_list:
+        shopping_list.remove(item)
         print "The item was removed"
-        return shoppin_list.sort()
-    elif item not in shoppin_list:
+        return shopping_list.sort()
+    elif item not in shopping_list:
         print "That item does not exist"
     
+def menu():
+    menu_1 = """
+    0 - Main Menu
+    1 - Show current list
+    2 - Add an item to your shopping list
+    3 - Remove an item to your shopping list
+    4 - exit """
+    print "Hello, This is your menu for the Shopping List. What do you want to do:?"
+    option_1 = (raw_input(menu_1))
+    return option_1    
 
 def main():
-    item = "avocado"
-    item1 = "arugula"
-    item2 = "beans"
-    item3 = "pizza"
-    add(item)
-    print shoppin_list
-    add(item1)
-    print shoppin_list
-    add(item2)
-    print shoppin_list
-    add(item3)
-    print shoppin_list
-    add(item1)
-    print shoppin_list
+    option_1 = menu()
+    while True:
+        if option_1 == "1":
+            print shopping_list
+            option_1 = menu()
+        elif option_1 == "2":
+            add_item = raw_input("Please type your item or X if you're done: ")
+            if add_item.upper() == "X":
+                option_1 = menu()
+            else:
+                add(add_item)
+            print shopping_list
+        elif option_1 == "3":
+            remove_item = raw_input("Please type your item or X if you're done: ")
+            if remove_item.upper() == "X":
+                option_1 = menu()
+            else:
+                remove(remove_item)
+            print shopping_list
+        elif option_1 == "4":
+            break
 
-    remove(item)
-    print shoppin_list
-    remove(item1)
-    print shoppin_list
-    remove(item2)
-    print shoppin_list
-    remove(item)
-    print shoppin_list
-    remove(item3)
-    print shoppin_list
-    remove(item1)
-    print shoppin_list
+
 
 if __name__ == '__main__':
     main()
